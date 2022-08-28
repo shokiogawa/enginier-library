80c2d0c2695272c41b2007768dfc724be9cfb5c8
shokiogawa <shouki2578@gmail.com>
2022-08-28 16:33:27 +0900

    add

---
+++ src/data/microCMS.js
@@ -0,0 +1,20 @@
+// import fs from 'fs'
+// import { createClient } from 'microcms-js-sdk'
+const fs = require('fs')
+const { createClient } = require('microcms-js-sdk')
+require('dotenv').config();
+
+const client = createClient({
+  serviceDomain: process.env.NODE_ENV_DOMAIN,
+  apiKey: process.env.NODE_ENV_APIKEY,
+})
+async function createJsonFile() {
+  const categoryData = await client.getList({ endpoint: 'categories' })
+  const tagData = await client.getList({ endpoint: 'tag' })
+  const jsonCategory = JSON.stringify(categoryData, null, 2)
+  const jsonTag = JSON.stringify(tagData, null, 2)
+  fs.writeFileSync('./src/data/tag.json', jsonTag)
+  fs.writeFileSync('./src/data/category.json', jsonCategory)
+}
+
+createJsonFile()
